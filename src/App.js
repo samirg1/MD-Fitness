6298142f8289e94869cbad1972fd0609d5d083a6
Samir Gupta <srgupta@bigpond.com>
2022-11-22 15:17:02 +1100

    initial setup and app bar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Fragment, Component } from "react";
+import ResponsiveAppBar from "./AppBar";
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <ResponsiveAppBar />
+      </Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/AppBar.js
@@ -0,0 +1,183 @@
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import MenuIcon from "@mui/icons-material/Menu";
+import Settings from "@mui/icons-material/Settings";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Toolbar from "@mui/material/Toolbar";
+import Tooltip from "@mui/material/Tooltip";
+import Typography from "@mui/material/Typography";
+import { Component } from "react";
+
+const pages = ["Products", "Pricing", "Blog"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+class ResponsiveAppBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      anchorElNav: null,
+      anchorElUser: null,
+    };
+  }
+
+  handleOpenNavMenu(event) {
+    this.setState({ anchorElNav: event.currentTarget });
+  }
+
+  handleOpenUserMenu = (event) => {
+    this.setState({ anchorElUser: event.currentTarget });
+  };
+
+  handleCloseNavMenu() {
+    this.setState({ anchorElNav: null });
+  }
+
+  handleCloseUserMenu() {
+    this.setState({ anchorElUser: null });
+  }
+
+  render() {
+    return (
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <FitnessCenterIcon
+              sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
+            />
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              MyGym
+            </Typography>
+
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={(e) => this.handleOpenNavMenu(e)}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={this.state.anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(this.state.anchorElNav)}
+                onClose={() => this.handleCloseNavMenu()}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem
+                    key={page}
+                    onClick={() => this.handleCloseNavMenu()}
+                  >
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+            <FitnessCenterIcon
+              sx={{ display: { xs: "flex", md: "none" }, mr: 1 }}
+            />
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href=""
+              sx={{
+                mr: 2,
+                display: { xs: "flex", md: "none" },
+                flexGrow: 1,
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              LOGO
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+              {pages.map((page) => (
+                <Button
+                  key={page}
+                  onClick={() => this.handleCloseNavMenu()}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton
+                  onClick={(e) => this.handleOpenUserMenu(e)}
+                  sx={{ p: 0 }}
+                >
+                  <Settings sx={{ color: "white" }} />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={this.state.anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(this.state.anchorElUser)}
+                onClose={() => this.handleCloseUserMenu()}
+              >
+                {settings.map((setting) => (
+                  <MenuItem
+                    key={setting}
+                    onClick={() => this.handleCloseUserMenu()}
+                  >
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    );
+  }
+}
+export default ResponsiveAppBar;
