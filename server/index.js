dd4d3861a8ed0cb62f18d6e18d9d677173a3eada
Samir Gupta <srgupta@bigpond.com>
2022-12-05 19:44:14 +1100

    connecting to db

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 const PORT = process.env.PORT || 3001;
+
+
+// connect to db
+mongoose.connect(process.env.DB_CONNECTION_URL, () => {
+    console.log("Connected to database");
+});
 
 //routes
 const authenticationRoute = require('./routes/authentication');
