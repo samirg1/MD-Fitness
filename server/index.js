8f75a6886a98b7e04c983e4f334c365aca2e988a
Samir Gupta <srgupta@bigpond.com>
2022-12-05 19:10:40 +1100

    start auth

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.get("/api", (_, res) => {
-    res.json({ message: "Hello from server!" });
-});
+//routes
+const authenticationRoute = require('./routes/authentication');
+
+// middles
+app.use('/api/user', authenticationRoute);
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
