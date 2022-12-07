324bd7a5dfd655971feac5e6129124a67c53f501
Samir Gupta <srgupta@bigpond.com>
2022-12-07 12:52:32 +1100

    get jwt token

--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const jsonwebtoken = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 
@@ -7,7 +8,7 @@ router.post("/register", async (req, res) => {
     if (validationError) return res.status(400).send(validationError.message);
 
     const emailExists = await User.findOne({ email: req.body.email });
-    if (emailExists) return res.status(400).send("Email already exists");
+    if (emailExists) return res.status(400).send("email already exists");
 
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
@@ -26,6 +27,21 @@ router.post("/register", async (req, res) => {
     }
 });
 
-router.post("/login", async (req, res) => {});
+router.post("/login", async (req, res) => {
+    const errorMessage = "invalid email and/or password"
+
+    if (User.validateLogin(req.body)) return res.status(400).send(errorMessage);
+
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send(errorMessage);
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).send(errorMessage);
+
+    const token = jsonwebtoken.sign({ name: user.name, email: user.email }, process.env.TOKEN_SECRET);
+    res.header('authentication-token', token).send(token);
+
+    res.send("Logged in as " + user.name);
+});
 
 module.exports = router;
