60302458ac3db9ea27035df76431ee7a6118cacc
Samir Gupta <srgupta@bigpond.com>
2022-12-16 00:12:43 +1100

    better jest config

--- client/jest.config.js
+++ client/jest.config.js
@@ -6,4 +6,16 @@ module.exports = {
         "\\.(css|less)$": "<rootDir>/src/tests/__mocks__/styleMock.ts",
     },
     testEnvironment: "jsdom",
+    collectCoverage: true,
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: -10,
+        },
+    },
+    verbose: true,
+    notify: true,
+    notifyMode: "always"
 };
