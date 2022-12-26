05a3ed16202ceb8642e1f69a70b1ad4cc42da4d9
Samir Gupta <srgupta@bigpond.com>
2022-12-27 01:56:42 +1100

    config conversions

--- server/config/allowedOrigins.js
+++
@@ -1,6 +0,0 @@
-/**
- * The allowed origins to call the server from.
- */
-const allowedOrigins = ["http://localhost:3000", "http://192.168.8.199:3000"];
-
-module.exports = allowedOrigins;
---
+++ server/config/allowedOrigins.ts
@@ -0,0 +1,4 @@
+/**
+ * The allowed origins to call the server from.
+ */
+export default ["http://localhost:3000", "http://192.168.8.199:3000"];
